b402670e2a4a8f8d3f60a529e9517f374bc05b38
Alberto Guaman <80141815+GbrielGarcia@users.noreply.github.com>
2023-07-18 23:15:02 -0500

    contrasena random

---
+++ contra.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+def generar_contrasena(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return contrasena
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from corazon import corazon
+# from corazon import corazon
+from contra import generar_contrasena
 
 
 def main():
-    corazon()
-
+    # corazon()
+    longitud_contrasena = 5 # Cambia esta variable para ajustar la longitud de la contraseña generada
+    contrasena_generada = generar_contrasena(longitud_contrasena)
+    print("Contraseña generada:", contrasena_generada)
 
 if __name__ == "__main__":
     main()
